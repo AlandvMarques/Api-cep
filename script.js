822f2fee10f277ee9132a3afe2b90a5b5ac2cee7
AlandvMarques <alandvmarques@gmail.com>
2024-02-20 21:58:23 -0300

    editar

--- script.js
+++ script.js
@@ -1,142 +1,41 @@
-const addressFrom = document.querySelector("#address-from");
-const cepInput = document.querySelector("#cep"); 
-const addressInput = document.querySelector("#address");
-const cityInput = document.querySelector("#city");
-const neighborhoodInput = document.querySelector("#neighborhood");
-const regionInput = document.querySelector("#region");
-const formInput = document.querySelectorAll("[data-input]");
+const cep = document.querySelector('#cep');
+const address = document.querySelector('#address');
+const bairro = document.querySelector('#bairro');
+const cidade = document.querySelector('#cidade');
+const message = document.querySelector('#message');
 
+cep.addEventListener('focusout', async ()=> {
+    
+    try {
+        const onlyNumbers = /^[0-9]+$/;
+        const cepValid = /^[0-9]{8}$/;
 
-const fadeElement = document.querySelector("#fade");
+        if(!onlyNumbers.test(cep.value) || !cepValid.test(cep.value)){
+            throw{cep_error: 'Cep invalid'}
+        }
 
-const closeButton = document.querySelector("#close-message");
+        const response = await fetch (`https://viacep.com.br/ws/${cep.value}/json/`);
 
-// Validar CEP input
-
-cepInput.addEventListener("keypress", (e)=> {
-    const onlyNumbers = /[0-9]/;
-    const key = String.fromCharCode(e.keyCode);
-
-
-    // allow only numbers
-
-    if(!onlyNumbers.test(key)){
-        e.preventDefault();
-        return;
-    }
-});
-
-// Get addres event
-
-cepInput.addEventListener("keyup", (e) =>{
-
-    const inputValue = e.target.value;
-
-    //Check if we have the correct length
-    if (inputValue.length === 8 ){
-        getAddress(inputValue);
-    }
-});
-
-
-// Get customer address from API
-
-const getAddress = async (cep) => {
-    toggleLoader();
-
-    cepInput.blur();
-
-    const apiUrl = `https;//viacep.com.br/ws/${cep}/json`;
-
-    const response = await fetch(apiUrl);
-
-    const data = await response.json();
-
-    //show error e rest from 
-
-    if(data.erro === "true"){
-
-        if(!addressInput.hasAttribute("disabled")){
-            toggleDisable();
+        if(!response.ok){
+            throw await response.json();
         };
 
-        addressFrom.reset();
-        toggleLoader();
-       toggleMessage("CEP invaálido, tente novamente.")
-        return;
+        const responseCep = await response.json();
+
+        address.value = responseCep.logradouro;
+        bairro.value = responseCep.bairro;
+        cidade.value = responseCep.localidade;
+
+
+    } catch (error) {
+        if(error?.cep_error){
+            message.textContent = error.cep_error;
+            setTimeout(()=>{
+                message.textContent ='';
+            },5000);
+        }
+
+        console.log(error);
     }
-    if(addressInput.value === ""){
-        toggleDisable();
-    };
 
-    addressInput.value = data.logradrouro;
-    cityInput.value = data.localidade;
-    neighborhoodInput.value = data.bairro;
-    regionInput.value = data.uf;
-
-
-    toggleLoader()
-};
-
-//add or remove disabled attribute
-
-const toggleDisable = () =>{
-    if (regionInput.hasAttribute("disabled")){
-        formInput.forEach((input) => {
-            input.removeAttribute("disabled");
-        });
-    }else{
-        formInputs.forEach((input) => {
-            input.setAttribute("disable", "disabled");
-        });
-    }
-};
-
-
-
-// show or hide loader 
-
-const toggleLoader = () => {
-    const loaderElement = document.querySelector("#loader");
-
-    fadeElement.classList.toggle("hide");
-    loaderElement.classList.toggle("hide");
-
-};
-
-
-//show or hide message 
-
-const toggleMessage = (msg) =>{
-    const messageElement = document.querySelector("#message");
-
-    const messageElementText = document.querySelector("#message p");
-
-    messageElementText.innerText = msg;
-
-    fadeElement.classList.toggle("hide");
-    messageElement.classList.toggle("hide");
-}
-
-//close message modal 
-
-closeButton.addEventListener("click", () => toggleMessage());
-
-
-// Save address: tem que ser modificado para enviar par api
-
-addressFrom.addEventListener("submit", (e) => {
-    e.preventDefault();
-
-    toggleLoader();
-
-    setTimeout(()=>{
-        toggleLoader();
-
-        toggleMessage("Endereço salvo com sucesso!");
-
-        addressFrom.reset();
-
-        toggleDisable();
-    }, 1500);
-});
+})
